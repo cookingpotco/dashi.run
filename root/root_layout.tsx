59bace0f4394e35302ae409c79b817aace88900a
Daniel Mikhailov <danielmkhlv@gmail.com>
2026-09-05 12:47:29 +0100

    Add favicon

--- root/root_layout.tsx
+++ root/root_layout.tsx
@@ -12,6 +12,11 @@ export function RootLayout({ ctx, children }: LayoutArgs): Element {
         <title>Dashi Web Framework</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
+          rel="icon"
+          href="/static/favicon.ico"
+          type="image/x-icon"
+        />
+        <link
           rel="preload"
           href="/static/plus-jakarta-sans.woff2"
           as="font"
